44285d296768e55a3936886e02cf0ae3711d530a
fitabigail <abidinu35@gmail.com>
2022-08-18 08:03:57 +0000

    Set up structure functions

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore()
+{
+
+}
+
+function incrementWrongAnswer()
+{
+
+}
+
+function displayAdditionQuestion()
+{
+
+}
+
+function displaySubstractQuestion(){
+
+}
+
+function displayMultiplayQuestion()
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
