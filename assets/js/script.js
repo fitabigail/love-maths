58610e33c13eab91a649fd2d24f5be41dc8222e7
fitabigail <abidinu35@gmail.com>
2022-08-19 08:39:25 +0000

    Add addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,23 +12,32 @@ document.addEventListener("DOMContentLoaded", function()
 
           else{
             let gameType = this.getAttribute("data-type");
-            alert('You clicked $({gameType}');
+            runGame(gameType);
           }
         }
        )
 
-
     }
+    runGame("addition");
 })
 
 /**
  * The main game "loop" called when the script is first loaded and
  *  after the user answer has been processeed
  */
-function runGame(){
+function runGame(gameType){
 
     let num1 = Math.floor(Math.random() * 25) +1;
     let num2 = Math.floor(Math.random() * 25) +1;
+
+    if (gameType === "addition")
+    {
+      displayAdditionQuestion(num1, num2);
+    } 
+    else{
+      alert('unknown game type: ${gameType}');
+      throw 'unkmow game type: ${gameType}. Aborting!'
+    }
 
 }
 
@@ -50,9 +59,11 @@ function incrementWrongAnswer()
 
 }
 
-function displayAdditionQuestion()
+function displayAdditionQuestion(operand1, operand2)
 {
-
+  document.getElementById("operand1").textContent = operand1;
+  document.getElementById("operand2").textContent = operand2;
+  document.getElementById("operand").textContent = "+";
 }
 
 function displaySubstractQuestion(){
