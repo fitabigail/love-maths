81d14126379d22825d34fa3862f7540eeaab59af
fitabigail <abidinu35@gmail.com>
2022-08-19 14:36:55 +0000

    Add division question, favicon and tidy up the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,12 @@ document.addEventListener("DOMContentLoaded", function() {
       });
   }
 
+  document.getElementById("answer-box").addEventListener("keydown", function(event)
+  {
+    if(event.key === "Enter"){
+      checkAnswer();
+    }
+  });
   runGame("addition");
 
 });
@@ -22,6 +28,9 @@ document.addEventListener("DOMContentLoaded", function() {
 * and after the user's answer has been processed
 */
 function runGame(gameType) {
+
+  document.getElementById("answer-box").value = "";
+  document.getElementById("answer-box").focus();
 
   // Creates two random numbers between 1 and 25
   let num1 = Math.floor(Math.random() * 25) + 1;
@@ -33,7 +42,9 @@ function runGame(gameType) {
       displayMultiplyQuestion(num1, num2);
   } else if (gameType === "subtract" ) {
       displaySubtractQuestion(num1, num2);
-  } else {
+  } else if (gameType === "division" ) {
+    displayDivisionQuestion(num1, num2);
+}else {
       alert(`Unknown game type: ${gameType}`);
       throw `Unknown game type: ${gameType}. Aborting!`;
   }
@@ -78,6 +89,8 @@ function calculateCorrectAnswer() {
       return [operand1 * operand2, "multiply"];
   } else if (operator === "-") {
       return [operand1 - operand2, "subtract"];
+  }else if (operator === "/") {
+    return [operand1 / operand2, "division"];
   } else {
       alert(`Unimplemented operator ${operator}`);
       throw `Unimplemented operator ${operator}. Aborting!`;
@@ -126,5 +139,12 @@ function displayMultiplyQuestion(operand1, operand2) {
   document.getElementById('operand1').textContent = operand1;
   document.getElementById('operand2').textContent = operand2;
   document.getElementById('operator').textContent = "x";
+
+}
+
+function displayDivisionQuestion(operand1, operand2){
+  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = "/";
 
 }
